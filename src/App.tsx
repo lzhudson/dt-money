01f009f2c2a67d17cbdd464033b6e119ad726749
Hudson Holanda <hudsonholanda55@gmail.com>
2021-12-18 11:04:11 -0300

    feat: import Header and GlobalStyle component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/styles/global";
 
 
 export function App() {
   return (
-   <h1>Hello World</h1>
+   <>
+     <Header />
+     <GlobalStyle />
+   </>
   );
 }
 
