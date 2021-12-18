74154fc5471e2d9b36c305084313f0ef4283856d
Hudson Holanda <hudsonholanda55@gmail.com>
2021-12-18 11:48:21 -0300

    feat: create TransactionsTable component

---
+++ src/TransactionsTable/index.tsx
@@ -0,0 +1,48 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return(
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="title">
+              Desenvolvimento de Website
+            </td>
+            <td className="deposit">
+              R$ 12000,00
+            </td>
+            <td>
+              Desenvolvimento
+            </td>
+            <td>
+              20/02/2021
+            </td>
+          </tr>
+          <tr>
+            <td className="title">
+              Aluguel Apartamento
+            </td>
+            <td className="withdraw">
+              - R$ 2000,00
+            </td>
+            <td>
+              Despesa
+            </td>
+            <td>
+              10/02/2021
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
