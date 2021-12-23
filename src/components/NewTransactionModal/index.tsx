853a98e1fef384c86a6701df57a435279fa872c6
Hudson Holanda <hudsonholanda55@gmail.com>
2021-12-23 09:50:31 -0300

    refact: change path to useTransactions hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,7 +4,7 @@ import closeImg from "../../assets/close.svg";
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import { FormEvent, useContext, useState } from "react";
-import { TransactionsContext } from "../../TransactionsContexts";
+import { useTransactions } from "../../hook/useTransactions";
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -14,7 +14,7 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState('');
   const [value, setValue] = useState(0);
